1e32c1010c5d535a4362ff1a23e7df7ccb0362d9
cawfree <hello@cawfree.com>
2020-04-22 20:28:22 +0100

    Extended unit sanitization.

--- index.js
+++ index.js
@@ -22,6 +22,24 @@ const sanitizePassword = (p) => {
 const sanitizeUnits = (units) => {
   if (units === "wei" || units === "WEI") {
     return "WEI";
+  } else if (units === "kwei" || units === "KWEI") {
+    return "KWEI";
+  } else if (units === "mwei" || units === "MWEI") {
+    return "MWEI";
+  } else if (units === "gwei" || units === "GWEI") {
+    return "GWEI";
+  } else if (units === "szabo" || units === "SZABO") {
+    return "SZABO";
+  } else if (units === "finney" || units === "FINNEY") {
+    return "FINNEY";
+  } else if (units === "ether" || units === "ETHER") {
+    return "ETHER";
+  } else if (units === "kether" || units === "KETHER") {
+    return "KETHER";
+  } else if (units === "mether" || units === "METHER") {
+    return "METHER";
+  } else if (units === "gether" || units === "GETHER") {
+    return "GETHER";
   }
   throw new Error(`Unsupported unit of eth, \"${units}\".`);
 };
